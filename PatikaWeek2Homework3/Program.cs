619f5d2c97cbafc4f5103b7581e317720ad9ab4d
aysegulgules <159700271+aysegulgules@users.noreply.github.com>
2024-07-21 15:07:52 +0300

    Add project files.

---
+++ PatikaWeek2Homework3/Program.cs
@@ -0,0 +1,34 @@
+﻿
+int i = 0;
+
+Console.WriteLine("Kaç defa \" Ben bir Patika'lıyım \" yazılsın? ");
+int limit = int.Parse(Console.ReadLine())-1;
+
+//while örneği
+Console.WriteLine("\n while-------------\n");
+while (i<=limit)
+{
+    Console.WriteLine($"{i + 1}- Ben bir Patika'lıyım");
+    i++;
+}
+
+
+
+//do-while örneği
+
+Console.WriteLine("\n do-while-------------\n");
+i = 0;
+do
+{
+    Console.WriteLine($"{i + 1}- Ben bir Patika'lıyım");
+    i++;
+}
+while (i <= limit);
+
+
+/*
+ "while" da koşul true değil ise ekran çıktısı  yazılmamakta.
+
+Ama "do-while" da koşul sonda olduğu için ekran çıktısı bir defa yazılmakta.
+ 
+ */
